210e13866fab2568420f109bdce143985dfa7708
rahul <rahulkawale18@gmail.com>
2023-07-11 22:34:29 +0530

    First commit

---
+++ src/com/velocity/GitExample.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+public class GitExample {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("first git project");
+		
+	}
+
+}
---
+++ src/com/velocity/hdfc/HdfcDemo.java
@@ -0,0 +1,10 @@
+package com.velocity.hdfc;
+
+public class HdfcDemo {
+
+	public static void main(String[] args) {
+		System.out.println("this is hdfc");
+	}
+	
+	
+}
