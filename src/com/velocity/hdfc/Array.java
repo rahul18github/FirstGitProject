7b7fc366b5cae3c827c4a67b48b9ee3e524bac78
rahul <rahulkawale18@gmail.com>
2023-07-13 08:26:21 +0530

    addes code for arraylist

---
+++ src/com/velocity/hdfc/Array.java
@@ -0,0 +1,18 @@
+package com.velocity.hdfc;
+
+import java.util.ArrayList;
+
+public class Array {
+	
+	public static void main(String[] args) {
+		
+		ArrayList al=new ArrayList();
+		
+		al.add(55);
+	    al.add(null);
+	    al.add("hello");
+	    
+	    System.out.println(al);
+	}
+
+}
